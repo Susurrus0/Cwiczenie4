c5b522eef476bd4db5d3ddd617a55ed564965f84
Susurrus0 <104753102+Susurrus0@users.noreply.github.com>
2025-11-29 17:50:58 +0100

    Poprawa wcięć

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 # Główny plik aplikacji
 # Autor: [Twoje Imię]
+
 def wyswietl_menu():
     """Funkcja wyświetlająca główne opcje programu"""
-print("--- APLIKACJA FINANSOWA v1.0 ---")
-print("1. Dodaj nowy wydatek")
-print("2. Wyświetl raport")
-print("3. Wyjście")
+    print("--- APLIKACJA FINANSOWA v1.0 ---")
+    print("1. Dodaj nowy wydatek")
+    print("2. Wyświetl raport")
+    print("3. Wyjście")
+
 def start_aplikacji():
-# TODO: Dodać obsługę logowania użytkownika
+    # TODO: Dodać obsługę logowania użytkownika
     print("Uruchamianie modułów...")
-wyswietl_menu()
+    wyswietl_menu()
+
 # Punkt startowy programu
 if __name__ == "__main__":
     start_aplikacji()
